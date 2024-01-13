2d27dba9e63df00911cabd378230f700e246474a
babimtero <babak.izadpanah@gmail.com>
2024-01-14 00:40:18 +0330

    second one

--- __main__.py
+++ __main__.py
@@ -7,6 +7,8 @@ Created on Sat Jan 13 23:13:08 2024
 
 import sqlite3 as sq
 import pandas as pd
+import mp4_2_wav as mw
+import os
 
 sqliteConnection = sq.connect('config.db')
 
@@ -14,4 +16,21 @@ sqliteConnection = sq.connect('config.db')
 sqlite_select_query = """SELECT * from init"""
 df=pd.read_sql(sqlite_select_query,sqliteConnection)
 
+#
 
+
+
+
+# Get the list of all files and directories
+path = df['video_url'][0]
+dir_list = os.listdir(path)
+ 
+print("Files and directories in '", path, "' :")
+ 
+# prints all files
+print(dir_list)
+
+for files in dir_list:
+    vfile=path+"/"+files
+    print(vfile)
+    mw.convert_video_to_audio_moviepy(vfile,df['audio_url'][0])
--- mp4_2_wav.py
+++ mp4_2_wav.py
@@ -8,12 +8,13 @@ Created on Fri Dec 22 16:10:01 2023
 import os
 from moviepy.editor import VideoFileClip
 
-def convert_video_to_audio_moviepy(video_file, output_ext="wav"):
+def convert_video_to_audio_moviepy(video_file,audio_url, output_ext="wav"):
     """Converts video to audio using MoviePy library
     that uses `ffmpeg` under the hood"""
     filename, ext = os.path.splitext(video_file)
     clip = VideoFileClip(video_file)
-    clip.audio.write_audiofile(f"{filename}.{output_ext}")
+    names=os.path.basename(filename).split('/')[-1]
+    clip.audio.write_audiofile(audio_url+"/"+f"{names}.{output_ext}")
   
 
  
