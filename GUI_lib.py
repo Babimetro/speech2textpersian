1cf705e5e55943eb6721c5d37d7176b19bc36bce
babimtero <babak.izadpanah@gmail.com>
2024-02-08 15:03:57 +0330

    last seassion

---
+++ GUI_lib.py
@@ -0,0 +1,79 @@
+import PySimpleGUI as sg
+
+ 
+
+def filebrowser():
+
+    sg.theme("DarkTeal2")
+
+    layout = [[sg.T("")],
+
+              [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-")],
+
+              [sg.Button("Submit")]]
+
+    ###Building Window
+
+    window = sg.Window('File Selection', layout, size=(600,150))
+
+       
+
+    while True:
+
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event=="Exit":
+
+            break
+
+        elif event == "Submit":
+
+            #print(values["-IN-"])
+
+            window.close()
+
+            return (values["-IN-"])
+
+ 
+
+ 
+
+ 
+
+ 
+
+def folderbrowser():
+
+    sg.theme("DarkTeal2")
+
+    layout = [[sg.T("")], [sg.Text("Choose a folder: "), sg.Input(), sg.FolderBrowse(key="-IN-")],[sg.Button("Submit")]]
+
+   
+
+    ###Building Window
+
+    window = sg.Window('Folder Selection', layout, size=(600,150))
+
+       
+
+    while True:
+
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event=="Exit":
+
+            break
+
+        elif event == "Submit":
+
+           #print(values["-IN-"])
+
+            window.close()  
+
+            return (values["-IN-"])
+
+           
+
+ 
+
+ 
--- __main__.py
+++ __main__.py
@@ -10,66 +10,127 @@ Created on Sat Jan 13 23:13:08 2024
 #    Liberaries
 #
 #================================
-
-try:
-    os.chdir('D:\git\stt')
-    print(os.getcwd())
-except:
-    pass
-
-
 import sqlite3 as sq
 import pandas as pd 
 import os
 import Functions as mw
 from Functions import lent
+import PySimpleGUI as sg
+import GUI_lib as gui
+#===============================
+#
+#      
+#    Gui
+#
+#================================
+
+frame_layout = [[sg.Multiline("", size=(80, 20), autoscroll=True,
+
+    reroute_stdout=True, reroute_stderr=True, key='-OUTPUT-')]]
+
+ 
+
+layout=[[sg.Text('My window')],
+
+        [sg.Button('Video Folder Location'),sg.Button('Audio Folder Location'),sg.Button('Voice Folder Location'),sg.Button('Close')],
+
+        [sg.Frame("Output console", frame_layout)],
+        [sg.Button('Video to Audio'),sg.Button('Audio Slicing'),sg.Button('Converting')]
+
+       
+
+                
+
+        ]
+
+window=sg.Window("My application",layout,size=(400,400))
+
+ 
+
+while True:
+
+    event,values=window.read()
+
+    if event in (None,'Cancel'):
+
+        window.close()
+
+    elif event=='Close':
+
+        window.close()
+
+    elif event=='Video Folder Location':
+
+        vf=gui.folderbrowser()
+
+        print('Video Folder Location='+vf)
+    elif event=='Audio Folder Location':
+
+        af=gui.folderbrowser()
+
+        print('Audio Folder Location'+af)
+        
+    elif event=='Voice Folder Location':
+
+        chf=gui.folderbrowser()
+
+        print('Voice Folder Location'+chf)
+    elif event=='Voice Folder Location':
+
+        chf=gui.folderbrowser()
+
+        print('Voice Folder Location'+chf)
+        
+    elif event=='Video to Audio':
+    
+        vfilelist=[]
+        for path, subdirs, files in os.walk(vf):
+            for name in files:
+                print(os.path.join(path, name))
+                vfilelist.append(os.path.join(path, name))
+        
+        for vnf in vfilelist:
+            mw.convert_video_to_audio_moviepy(vnf, af)
 
 
-try:
-    os.chdir('D:\git\stt')
-    print(os.getcwd())
-except:
-    pass
+        print('Voice Folder Location'+chf)
+        
+    elif event=='Voice Folder Location':
+
+        afls=[]
+        for path, subdirs, files in os.walk(af):
+            for name in files:
+                print(os.path.join(path, name))
+                afls.append(os.path.join(path, name))
+
+        for anf in afls:
+            #mw.audio_word_spliter(anf,chf)
+            #split_in_parts(anf, chf)
+            lent(anf,15,chf)
 
 
-sqliteConnection = sq.connect('config.db')
+    elif event=='Voice Folder Location':
+ 
+        stts=[]
+        for path, subdirs, files in os.walk(chf):
+            for name in files:
+                print(os.path.join(path, name))
+                stts.append(os.path.join(path, name))
+        
+        for stt in stts:
+            #mw.audio_word_spliter(anf,chf)
+            #split_in_parts(anf, chf)
+            mw.sttv(stt, "fa")
+   
+#===============================
+#
+#      
+#    Collect input file list
+#
+#================================
 
 
-sqlite_select_query = """SELECT * from init"""
-df=pd.read_sql(sqlite_select_query,sqliteConnection)
-vf=df['video_url'][0]
-af=df['audio_url'][0]
-chf=df['voice_url'][0]
-vfilelist=[]
-for path, subdirs, files in os.walk(vf):
-    for name in files:
-        print(os.path.join(path, name))
-        vfilelist.append(os.path.join(path, name))
 
-for vnf in vfilelist:
-    mw.convert_video_to_audio_moviepy(vnf, af)
-
-afls=[]
-for path, subdirs, files in os.walk(af):
-    for name in files:
-        print(os.path.join(path, name))
-        afls.append(os.path.join(path, name))
-
-for anf in afls:
-    #mw.audio_word_spliter(anf,chf)
-    #split_in_parts(anf, chf)
-    lent(anf,15,chf)
-
-stts=[]
-for path, subdirs, files in os.walk(chf):
-    for name in files:
-        print(os.path.join(path, name))
-        stts.append(os.path.join(path, name))
-
-for stt in stts:
-    #mw.audio_word_spliter(anf,chf)
-    #split_in_parts(anf, chf)
-    mw.sttv(stt, "fa")
 
 '''
 # Get the list of all files and directories
@@ -86,3 +147,6 @@ for files in dir_list:
     print(vfile)
     mw.convert_video_to_audio_moviepy(vfile,df['audio_url'][0])
 '''
+ 
+
+window.close()
