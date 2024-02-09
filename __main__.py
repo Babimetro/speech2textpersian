56e39f0e0a237cd1475e40ae524457bb4532f9eb
babimtero <babak.izadpanah@gmail.com>
2024-02-09 10:53:44 +0330

    last seassion Gui Added

--- __main__.py
+++ __main__.py
@@ -34,7 +34,7 @@ frame_layout = [[sg.Multiline("", size=(80, 20), autoscroll=True,
 
 layout=[[sg.Text('Online STT')],
 
-        [sg.Button('Video Folder Location'),sg.Button('Audio Folder Location'),sg.Button('Voice Folder Location')],
+        [sg.Button('Video Folder Location'),sg.Button('Audio Folder Location'),sg.Button('Voice Folder Location'),sg.Spin(['5','10','15','20','25','30'], s=(15,2))],
 
         [sg.Frame("Output console", frame_layout)],
         [sg.Button('Video to Audio'),sg.Button('Audio Slicing'),sg.Button('STT'),sg.Button('Close')],
@@ -48,11 +48,11 @@ layout=[[sg.Text('Online STT')],
 window=sg.Window("Video STT-Babi",layout,size=(500,550))
 #window['-OUT-'].update("0%")
  
-
+#window['-PBAR-'].update(max=100)
 while True:
 
     event,values=window.read()
-
+    
     #=================Folder Configuration===================
     #=======================Video============================
     if event=='Video Folder Location':
@@ -76,54 +76,73 @@ while True:
     #=================Buttons=====================================
     #====================Video t Audio Convertor==================
     elif event=='Video to Audio':
+        i=0
+        window['-OUT-'].update(str(i)+"%")
+        window['-PBAR-'].update(current_count=i )
+        
         vfilelist=[]
         for path, subdirs, files in os.walk(vf):
             for name in files:
                 print(os.path.join(path, name))
                 vfilelist.append(os.path.join(path, name))
         vff=vfilelist
-        i=0
-        window['-OUT-'].update(str(i)+"%")
         for vnf in vfilelist:
-
+            
             mw.convert_video_to_audio_moviepy(vnf, af)
             i=i+round(100/len(vfilelist),0)
-            window['-PBAR-'].update(current_count=i )
+            if i>100:
+                i=100
             window['-OUT-'].update(str(i)+"%")
+            window['-PBAR-'].update(current_count=i )
+            print('------------------------------')
+            print('-----------Finished-----------')
+            
+            
 
     #=================Buttons=====================================
     #=========================='Audio Slicing'====================       
     elif event=='Audio Slicing':
-
+        i=0
+        window['-OUT-'].update(str(i)+"%")
+        window['-PBAR-'].update(current_count=i )
         afls=[]
         for path, subdirs, files in os.walk(af):
             for name in files:
                 print(os.path.join(path, name))
                 afls.append(os.path.join(path, name))
-        i=0
-        window['-OUT-'].update(str(i)+"%")
+
         for anf in afls:
             i=i+round(100/len(afls),0)
-            window['-PBAR-'].update(current_count=i )
+            if i>100:
+                i=100
             window['-OUT-'].update(str(i)+"%")
+            window['-PBAR-'].update(current_count=i )
             lent(anf,15,chf)
+            print('------------------------------')
+            print('-----------Finished-----------')
     #=================Buttons=====================================
     #========================'Audio Slicing'====================    
 
     elif event=='STT':
- 
+        i=0
+        window['-OUT-'].update(str(i)+"%")
+        window['-PBAR-'].update(current_count=i )
         stts=[]
         for path, subdirs, files in os.walk(chf):
             for name in files:
                 print(os.path.join(path, name))
                 stts.append(os.path.join(path, name))
-        i=0
-        window['-OUT-'].update(str(i)+"%")       
+   
         for stt in stts:
             i=i+round(100/len(stts),0)
-            window['-PBAR-'].update(current_count=i )
+            if i>100:
+                i=100
             window['-OUT-'].update(str(i)+"%")
+            window['-PBAR-'].update(current_count=i )
+
             mw.sttv(stt, "fa")
+            print('------------------------------')
+            print('-----------Finished-----------')
     
     #=====================================================================
     #=====================================================================  
